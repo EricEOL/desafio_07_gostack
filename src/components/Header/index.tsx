614d7cdea21a6ba24f6ca05b4d306d00c1df19c6
EricEOL <lopes.eric051@gmail.com>
2020-11-22 19:40:57 -0300

    Desafio 07 terminado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
@@ -10,17 +10,41 @@ interface HeaderProps {
   size?: 'small' | 'large';
 }
 
-const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
-  <Container size={size}>
-    <header>
-      <img src={Logo} alt="GoFinances" />
-      <nav>
-        {
-          // Todo
-        }
-      </nav>
-    </header>
-  </Container>
-);
+const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => {
+  const [currentPage, setCurrentPage] = useState();
+
+  function verifyCurrentPage(): void {
+    setCurrentPage(window.location.pathname);
+  }
+  function modifyStyleOfMenuItem(): void {
+    const menuItems = document.querySelectorAll('header nav a');
+
+    menuItems.forEach(item => {
+      const hrefItem = item.getAttribute('href');
+
+      if (hrefItem === null) {
+        throw new Error('Panda null');
+      }
+
+      if (hrefItem.includes(currentPage)) {
+        item.innerHTML = 'Panda';
+      }
+    });
+  }
+
+  modifyStyleOfMenuItem();
+
+  return (
+    <Container size={size}>
+      <header onLoad={verifyCurrentPage}>
+        <img src={Logo} alt="GoFinances" />
+        <nav>
+          <Link to="/list">Listagem</Link>
+          <Link to="/import">Importar</Link>
+        </nav>
+      </header>
+    </Container>
+  );
+};
 
 export default Header;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
+    <Route path="/" exact render={() => <Redirect to="/list" />} />
+    <Route path="/list" component={Dashboard} />
     <Route path="/import" component={Import} />
   </Switch>
 );
